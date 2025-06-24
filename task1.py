b05ab6af75a249a561d8785bfd4d4b959c641f51
Moaaz69 <133379124+Moaaz69@users.noreply.github.com>
2025-06-24 09:47:03 +0100

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+int_input = int(input("Enter an integer: "))
+float_input = float(input("Enter a float: "))
+string_input = input("Enter a string: ")
+
+# Displaying
+
+print(f"The integer value is: {int_input}")
+print(f"The float value is: {float_input}")
+print(f"The string value is: {string_input}")
