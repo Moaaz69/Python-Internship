e317aee3ec46be6a55fecc2c79392dae26f2f37e
Moaaz69 <133379124+Moaaz69@users.noreply.github.com>
2025-06-27 08:02:11 +0100

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,22 @@
+#Student Record System with Tuples & Sets
+
+# Store student IDs using a tuple
+student_ids = (10, 11, 12, 13)
+print("Student IDs (tuple):", student_ids)
+
+# Create a set of unique course names
+courses = {"Python", "AI", "ML"}
+print("Initial courses (set):", courses)
+
+# Add a new course to the set
+courses.add("Data Science")
+print("After adding 'Data Science':", courses)
+
+# Removing  a course from the set
+courses.remove("AI")
+print("After removing 'AI':", courses)
+
+# Adding a course
+courses.add("Deep Learning")
+print("After adding 'Deep Learning':", courses)
+
